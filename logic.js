e931cefc3dfd8ffd05ef913950214a3bb2153def
jamima19 <143338662+jamima19@users.noreply.github.com>
2023-11-30 10:00:03 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,19 @@
+let inputs = document.getElementById("inp")
+let text = document.querySelector(".text")
+
+function Add(){
+    if(inputs.value == ""){
+        alert("please enter task")
+    }else{
+        let newElement = document.createElement("ul");
+        newElement.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i> `;
+        text.appendChild(newElement);
+        inputs.value="";
+        newElement.querySelector("i").addEventListener("click" ,remove);
+        function remove (){
+            newElement.remove()
+
+        }
+    }
+    
+}
